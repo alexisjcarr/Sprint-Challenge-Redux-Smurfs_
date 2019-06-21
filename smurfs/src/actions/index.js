35f669eab565573a1571b317ba8ddeae121bd034
alexisjcarr <acarr59@gmail.com>
2019-06-21 10:35:23 -0500

    Added requisite fetchSmurf cases in reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const FETCHING_SMURFS_PENDING = "FETCHING_SMURFS_PENDING";
-const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
-const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+export const FETCHING_SMURFS_PENDING = "FETCHING_SMURFS_PENDING";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
 
 export const fetchSmurfs = () => dispatch => {
   dispatch({ type: FETCHING_SMURFS_PENDING });
@@ -14,6 +14,9 @@ export const fetchSmurfs = () => dispatch => {
     })
     .catch(err => {
       console.log(err);
-      dispatch({ type: FETCHING_SMURFS_FAILURE });
+      dispatch({
+        type: FETCHING_SMURFS_FAILURE,
+        payload: "You smurfed up! No smurfs, fam :C"
+      });
     });
 };
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -11,6 +11,25 @@ const initialState = {
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionType.FETCHING_SMURFS_PENDING:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ""
+      };
+    case actionType.FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: "",
+        smurfs: action.payload
+      };
+    case actionType.FETCHING_SMURFS_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
     default:
       return state;
   }

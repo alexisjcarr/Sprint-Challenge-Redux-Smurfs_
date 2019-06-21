4e5a6d87acd99cbecefae50f9b93d303c196d86f
alexisjcarr <acarr59@gmail.com>
2019-06-21 10:45:23 -0500

    Smurfs rendering on screen

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 
 import "./App.css";
+import SmurfList from "./SmurfList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    smurfs: state.smurfs
-  };
+const App = () => {
+  return (
+    <div className="App">
+      <SmurfList />
+    </div>
+  );
 };
 
-export default connect(
-  mapStateToProps,
-  {}
-)(App);
+export default App;

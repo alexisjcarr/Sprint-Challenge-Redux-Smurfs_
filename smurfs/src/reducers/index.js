6889d8451a0460b000557ec9a407f1fad546f2cf
alexisjcarr <acarr59@gmail.com>
2019-06-21 12:06:08 -0500

    Added requisite deleteSmurf reducer cases

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -42,10 +42,28 @@ export const rootReducer = (state = initialState, action) => {
         addingSmurf: false,
         error: ""
       };
-    case action.ADD_SMURF_FAILURE:
+    case actionType.ADD_SMURF_FAILURE:
       return {
         ...state,
         addingSmurf: false,
+        error: action.payload
+      };
+    case actionType.DELETE_SMURF_PENDING:
+      return {
+        ...state,
+        deletingSmurf: true,
+        error: ""
+      };
+    case actionType.DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        deletingSmurf: false,
+        error: ""
+      };
+    case actionType.DELETE_SMURF_FAILURE:
+      return {
+        ...state,
+        deletingSmurf: false,
         error: action.payload
       };
     default:

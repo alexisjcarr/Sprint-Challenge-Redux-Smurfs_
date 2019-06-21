5d91930ed7f987f57758720864c768670cb2db58
alexisjcarr <acarr59@gmail.com>
2019-06-21 10:31:15 -0500

    Added fetchSmurfs action creator to actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,19 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from "axios";
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+const FETCHING_SMURFS_PENDING = "FETCHING_SMURFS_PENDING";
+const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS_PENDING });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCHING_SMURFS_FAILURE });
+    });
+};
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
       </div>
     );
   }
@@ -18,8 +15,11 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
+    smurfs: state.smurfs
+  };
+};
 
-  }
-}
-
-export default connect(mapStateToProps, {})(App);
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
